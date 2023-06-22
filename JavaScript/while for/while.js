f42c8c167de38a3ee90aeef9bcfc468fa154b452
gk <gk@adamant.local>
2023-06-22 21:11:41 +0300

    javascript

---
+++ JavaScript/while for/while.js
@@ -0,0 +1,67 @@
+/*let i=0;
+debugger;
+while (i<3) {
+    // выводит 0, затем 1, затем 2
+    console.log (i); i++;
+}
+
+let i=3;
+while (i!=0) { 
+    //когда i будет равно 0, условие станет ложным и цикл остановится
+    console.log (i); i--;
+}
+
+let i=0;
+debugger;
+do {
+    alert(i);
+    i++;
+} while(i<3);
+
+
+for (let i=0; i<3; i++) {
+    console.log("number", i);
+}
+for (let i=0; i<5; i++) alert(i);
+let i=0; while (i!=5) {alert(i++);}
+
+
+
+for (let i=2; i<=10; i++) {
+    if (i % 2==0) {
+        console.log(i);
+    }
+}
+
+let sum = 0;
+debugger
+for (let i=0; i<=100; i++) {
+    sum +=i;
+}
+console.log(sum);*/
+
+
+/*for (let i=1; i<=10; i++) {
+        console.log(i);
+    }
+
+    for (let i=20; i>=11; i--) {
+        console.log(i);
+    }
+
+    for (let i=1; i<=20; i++) {
+        if (i % 2==0)/*четные*/ /*(i % 2!=0) нечетные
+        { console.log(i);}
+    }*/
+
+let N=100;
+let sum = 0;
+for (let i=1; 1<=N; i++) {
+    if (i % 2==0) {
+        sum +=i
+    }
+}
+console.log(sum);
+
+
+  
