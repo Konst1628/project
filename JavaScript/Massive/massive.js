8af544cfe898f2cc49a842d85dcc2e2f4c3e957a
gk <gk@adamant.local>
2023-07-20 20:56:22 +0300

    javascript

--- JavaScript/Massive/massive.js
+++ JavaScript/Massive/massive.js
@@ -26,15 +26,67 @@ console.log(fruits3.length);
 
 let  fruits = ["Яблоко","Апельсин", "Груша"];
 alert(fruits.pop()); // удаляем "Груша" и выводим его 
-alert (fruits); //Яблоко, Апельсин */
+alert (fruits); //Яблоко, Апельсин 
 
 let fruits=["Яблоко","Апельсин"];
 fruits.push("Груша");
-alert(fruits);//Яблоко,Апельсин, Груша 
+alert(fruits);//Яблоко,Апельсин, Груша
 
-let fruits = ["Яблоко","Апельсин", "Слива"]
+let fruits = ["Яблоко","Апельсин", "Слива"];
 alert(fruits.length);//3
 
 let arr=[];
 arr[1000]= true;
-alert(arr.length); //1001
+alert(arr.length); //1001 
+
+
+let fruits = ["Яблоко","Апельсин", "Груша"];
+alert(fruits.shift());//удаляем Яблоко и выводим его 
+
+alert(fruits); //Апельсин, Груша
+
+
+let fruits = ["Апельсин", "Груша"];
+fruits.unshift('Яблоко');
+
+alert(fruits); //Яблоко,Апельсин, Груша
+
+var arr=[];
+alert(arr);
+for(i=0;i<10;i++)
+{
+    arr[i] =i+1;
+}
+console.log(arr); //[1,2,3,4,5,6,7,8,9,10]
+
+var arr = ["Яблоко","Апельсин","Груша"];
+for(i=0; i<arr.length;i++)
+{
+    alert(arr[i]);
+}
+
+
+let matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+alert(matrix[1][1]);//5, центральный элемент
+
+
+const array1= ['a','b','c'];
+const array2= ['d','e','f'];
+const array3 = array1.concat(array2);
+console.log(array3);
+ 
+
+const array1= ['one','two','three'];
+console.log('array1:', array1);
+const reversed=array1.reverse();
+console.log('reversed:',reversed);*/
+
+const elements=['Fire', 'Air', 'Water'];
+console.log(elements.join()); // Fire,Air,Water
+console.log(elements.join('')); // FireAirWater
+console.log(elements.join('-')); // Fire-Air-Water
---
+++ JavaScript/Massive/task.js
@@ -0,0 +1,33 @@
+/*const array1= ['1','2','3'];
+const array2= ['4','5','6'];
+const array3 = array1.concat(array2);
+console.log(array3);
+
+
+const array1= ['1','2','3'];
+const reversed=array1.reverse();
+console.log('reversed:',reversed);
+
+
+const arr = [1,2,3];
+arr.push(4,5,6);
+console.log(arr)
+
+let arr = ['1', '2','3'];
+arr.unshift('4','5','6');
+console.log(arr)
+
+let matrix = 
+    [7,9,3]
+;
+
+alert(matrix[1]);
+
+let matrix = 
+    [7,9,3];
+alert(matrix[2]);
+*/
+let massiv= [1,2,3];
+console.log(massiv[1]);
+console.log(massiv[massiv.length-1]);
+console.log(massiv.at(-1));
