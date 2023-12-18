47de7589c02c4ebafa6419f18550aeabe14782fa
gk <gk@adamant.local>
2023-12-18 20:42:32 +0300

    REACT

---
+++ app-new/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+//import Count from './Count';
+import Inut from './Inut'
+import ChangeNameSpread from './ChangeNameSpread';
+import AddCat from './AddCat';
+import Calculator from './Calculator';
+function App() {
+  return (
+    <Calculator/>
+    // <> <Count/>
+    // <Count/>
+    // </>
+  );
+}
+
+export default App;
